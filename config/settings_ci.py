a932f1ea436fb732410a57a6defb37d86cbf27a7
Aleksey Lozhkin <aleksey.s.lozhkin@gmail.com>
2026-06-02 00:03:03 +0300

    Disable Elasticsearch signals in CI

--- config/settings_ci.py
+++ config/settings_ci.py
@@ -1,19 +1,76 @@
-import os
+from pathlib import Path
 
-from .settings import INSTALLED_APPS  # noqa: F401
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-os.environ["ELASTICSEARCH_DSL_AUTO_REFRESH"] = "False"
-
-# Устанавливаем SECRET_KEY для CI
+# Безопасность
 SECRET_KEY = "django-insecure-ci-test-key-1234567890"
+DEBUG = True
+ALLOWED_HOSTS = ["*"]
 
-# Используем SQLite для CI тестов
+# Приложения
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework_simplejwt",
+    "drf_spectacular",
+    "django_htmx",
+    "users",
+    "documents",
+]
+
+# Мидлварь
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_htmx.middleware.HtmxMiddleware",
+]
+
+ROOT_URLCONF = "config.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "config.wsgi.application"
+
+# База данных SQLite для тестов
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": ":memory:",
     }
 }
+
+# Кастомная модель пользователя
+AUTH_USER_MODEL = "users.User"
+
+# Отключаем Elasticsearch
+ELASTICSEARCH_DSL = {
+    "default": {"hosts": "http://localhost:9200"},
+}
+ELASTICSEARCH_DSL_AUTO_REFRESH = False
 
 # Отключаем кэширование
 CACHES = {
@@ -22,14 +79,19 @@ CACHES = {
     }
 }
 
-# Отключаем Elasticsearch
-ELASTICSEARCH_DSL = {
-    "default": {"hosts": "http://localhost:9200"},
-}
-ELASTICSEARCH_DSL_AUTO_REFRESH = False
+# Статика
+STATIC_URL = "/static/"
+MEDIA_URL = "/media/"
 
-# Удаляем django_elasticsearch_dsl из INSTALLED_APPS
-INSTALLED_APPS = [app for app in INSTALLED_APPS if app != "django_elasticsearch_dsl"]
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# DRF
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": ("rest_framework_simplejwt.authentication.JWTAuthentication",),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
 
 # Логи в консоль
 LOGGING = {
@@ -45,3 +107,6 @@ LOGGING = {
         "level": "ERROR",
     },
 }
+
+# Email для тестов (не отправляет)
+EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
